d25eeb75c5b5f2abe9b1c87999a6412d3e758013
nitin reddy <reddynitin3@gmail.com>
2019-03-28 16:19:06 +0530

    Basic easy list

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords()
-    );
-  }
-}
-
-// #docregion RandomWordsState, RWS-class-only
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  // #enddocregion RWS-class-only
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  // #docregion RWS-class-only
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Easylist'),
+        )
       ),
     );
   }
-}
-// #enddocregion RandomWordsState, RWS-class-only
-
-// #docregion RandomWords
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
 }
