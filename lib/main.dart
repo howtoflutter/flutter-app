ab8dc198230c0e42050fc4f5a3af8e711ca2f3b1
nitin reddy <reddynitin3@gmail.com>
2019-03-31 23:03:45 +0530

    Added image under assets

--- lib/main.dart
+++ lib/main.dart
@@ -15,20 +15,30 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                new Text(
-                  "Hello World", 
-                  style: new TextStyle(
-                    color: Colors.pink,
-                    fontSize: 28
-                  )
-                ),
-                new Text("WOW"),
-              ],
+            // child: Column(
+            //   crossAxisAlignment: CrossAxisAlignment.end,
+            //   children: <Widget>[
+            //     new Text(
+            //       "Hello World", 
+            //       style: new TextStyle(
+            //         color: Colors.pink,
+            //         fontSize: 28
+            //       )
+            //     ),
+            //     new Padding(
+            //       padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+            //       child: new Text("WoW"),
+            //     ),
+            //     new Text("WOW"),
+            //   ],
+            // ),
+            child: Center(
+              child: Text("Hello Nitin"),
             ),
-
+            // child: Image(
+            //   // image:NetworkImage("https://www.sideshow.com/storage/product-images/9032461/batman_dc-comics_feature.jpg") ,
+            //   image: AssetImage("assets/lot/flash.jpg"),
+            // )
           ),
         ),
       ),
